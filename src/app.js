61ab24c66e2d0c3f7a2f4d5fca7fc9f2c66cdc47
Niniatura <neli.marcos@gmail.com>
2023-01-05 15:14:49 -0300

    Renderiza motor de plantillas handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartRouter from "./routes/cart.router.js";
+import { engine } from "express-handlebars";
+import path from "path";
+import {fileURLToPath} from 'url';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set('views', path.resolve(__dirname, 'views'));
+app.use(express.static('public'))
+
+app.get("/home", (req, res) =>{
+  res.render("prueba", {
+    title:"Hola",
+    message:"mensaje",
+    name:"Neli"
+  });
+});
+
 const PORT = 8080;
 app.use(express.urlencoded({extended: true} ));
 app.use(express.json());
-app.use(express.static("public"));
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
